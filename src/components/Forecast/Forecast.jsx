93de5dceca197563f246d0b6e4ac08a4aedb4db0
Revaz Tamoev <revaztamoev522@gmail.com>
2024-05-20 22:25:17 +0300

    restyle the project pt. 2

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -1,37 +1,19 @@
 import "./Forecast.css";
-import Clear from "../../assets/clear.png"
+import Clear from "../../assets/clear.png";
 export const Forecast = () => {
   return (
     <div className="forecast-container">
       <div className="forecast__content">
         <ul className="forecast__list">
           <li className="forecast__element">
-            <div className="forecast__box">
-              <h3 className="forecast__city">Moscow</h3>
-              <span>21.03.2024</span>
+            <h3 className="forecast__day">Monday</h3>
+            <div className="forecast__image">
+              <img src={Clear} alt="" />
             </div>
-
-            <div className="forecast__tempeture">
-							<img src={Clear} alt="" />
-              <h3>9°C</h3>
-            </div>
-						<div className="forecast__box">
-              <h3>53%</h3>
-							<span>Humidity</span> 
-            </div>
+            <h3 className="forecast__tempeture">9°C</h3>
           </li>
-          <li className="forecast__element">
-            <div className="forecast__box">
-              <h3 className="forecast__city">Moscow</h3>
-              <span>21.03.2024</span>
-            </div>
-          </li>
-          <li className="forecast__element">
-            <div className="forecast__box">
-              <h3 className="forecast__city">Moscow</h3>
-              <span>21.03.2024</span>
-            </div>
-          </li>
+         
+    
         </ul>
       </div>
     </div>
